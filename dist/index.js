51dc132dcad27f8ff180eaebb8297e95f7aabfae
tuca-janahu <arturjanahu@gmail.com>
2025-09-10 17:17:25 -0300

    feat: typescript

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+app.get('/tasks', (req, res) => {
+    res.json([
+        { id: 1, title: 'Task One', completed: false },
+        { id: 2, title: 'Task Two', completed: true }
+    ]);
+});
+//# sourceMappingURL=index.js.map
